2b803b988c6be9ffa45435221f57f91651ef05a3
Luv <luvmakin01@gmail.com>
2021-04-22 15:56:46 +0530

    leetcode - int to Roman

---
+++ intToRoman.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public String intToRoman(int num) {
+      int[] vals = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+      String[] roms = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+      StringBuilder ans = new StringBuilder();
+      
+      for(int i = 0; num > 0; i++)
+         while(num > vals[i]) {
+           ans.append(roms[i]);
+           num -= vals[i];
+         }
+      
+      return ans.toString();
+    }
+}
